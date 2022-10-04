e197bb3d68dea94390341fadde9dca4b078b68c3
spe358 <spe358@utexas.edu>
2022-10-03 20:32:25 -0500

    Didn't work :( reverting to img urls again

--- frontend/src/components/Planets/PlanetList.js
+++ frontend/src/components/Planets/PlanetList.js
@@ -20,8 +20,8 @@ import {
 const planet_list = [
   {
     name: "Mercury",
-    img: require('../../assets/planets/MercuryImg.jpeg'),
-    orbit_img: require('../../assets/planets/MercuryOrbit.jpeg'),
+    img: "https://scx2.b-cdn.net/gfx/news/hires/2015/whatsimporta.jpg",
+    orbit_img: "http://luna1.diviner.ucla.edu/~jpierre/mercury/posters/Poster-03/images/spin-orbit-resonance.jpg",
     mass: 0.000174,
     radius: 0.0341,
     period: 88.0,
