058aa5841d7364150efe97f4323199fee564d864
spe358 <spe358@utexas.edu>
2022-10-03 21:34:06 -0500

    Fixed dynamic gitlab stats

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,147 +1,159 @@
-import React, { useEffect, useState, version } from 'react'
-import { Row, Col, Card, ListGroup, ListGroupItem } from 'react-bootstrap'
-import teamInfo from "./TeamInfo.js"
+import React, { useEffect, useState } from "react";
+import { Row, Col, Card, Container, ListGroup, ListGroupItem, Table } from "react-bootstrap";
+import TeamInfo from "./TeamInfo.js"
 import toolInfo from "./ToolInfo.js"
 import APIInfo from "./APIInfo.js"
+import "../../styles/About.css";
 
 const api_version = "v4"
 const id = "39622546"
-const COMMITS_URL = `https://gitlab.com/api/${api_version}/projects/${id}/repository/commits`
-const ISSUES_URL = `https://gitlab.com/api/${api_version}/projects/${id}/issues`
 const REPO_URL = "https://gitlab.com/NathanSuss/group12-cs373"
 const POSTMAN_URL = "https://documenter.getpostman.com/view/20771905/2s83tFHWkc"
 
-// Adapted from Electrends https://gitlab.com/dandom25/electrends/
-const getGitLabInfo = async () => {
-    let totalCommitCount = 0, totalIssueCount = 0, totalTestCount = 0;
-    teamInfo.forEach((member) => {
-        member.issues = 0;
-        member.commits = 0;
-        member.tests = 0;
+// Adapted from Finding Footprints: https://gitlab.com/AlejandroCantu/group2
+const getGitLabStats = async () => {
+  let commitCount = 0;
+  let issueCount = 0;
+  let testCount = 0;
+
+  TeamInfo.forEach((member) => {
+    member.commits = 0;
+    member.issues = 0;
+    testCount += member.tests;
+  });
+
+  let commits = [],
+    page = [],
+    pageNum = 1;
+  do {
+    page = await fetch(
+      `https://gitlab.com/api/${api_version}/projects/${id}/repository/commits?per_page=100&page=${pageNum}&all=true`
+    );
+    page = await page.json();
+    commits = [...commits, ...page];
+    pageNum += 1;
+  } while (page.length === 100);
+  commits.forEach((commit) => {
+    const { author_name, author_email } = commit;
+    TeamInfo.forEach((member) => {
+      if (
+        member.name === author_name ||
+        member.username === author_name ||
+        member.email === author_email
+      ) {
+        member.commits += 1;
+      }
     });
+    commitCount += 1;
+  });
 
-    let commits = await fetch(COMMITS_URL)
-    commits = await commits.json()
-    totalCommitCount = commits.length
+  pageNum = 0;
+  page = [];
+  let issues = [];
+  do {
+    page = await fetch(
+      `https://gitlab.com/api/${api_version}/projects/${id}/issues?per_page=100&page=${pageNum}`
+    );
+    page = await page.json();
+    issues = [...issues, ...page];
+    pageNum += 1;
+  } while (page.length === 100);
 
-    commits.forEach((commit) => {
-        const { author_name, author_email } = commit
-        teamInfo.forEach((member) => {
-            if (member.name === author_name || author_name === member.username || member.username === author_name || member.email === author_email) {
-                member.commits += 1
-            }
-        })
-    })
+  issues.forEach((issue) => {
+    const { author } = issue;
+    const { name, username, author_email } = author;
+    TeamInfo.forEach((member) => {
+      if (
+        member.name === name ||
+        member.username === username ||
+        member.name === username
+      ) {
+        member.issues += 1;
+      }
+    });
+    issueCount += 1;
+  });
 
-    let issues = await fetch(ISSUES_URL)
-    issues = await issues.json()
-    totalIssueCount = issues.length
+  return {
+    teamMembers: TeamInfo,
+    numCommits: commitCount,
+    numIssues: issueCount,
+    numTests: testCount,
+  };
+};
 
-    issues.forEach((issue) => {
-        console.log(issue)
-        const { author } = issue
-        const { name, username } = author
-        teamInfo.forEach((member) => {
-            if (member.name === name || name === member.username || member.username === username || member.name === username) {
-                member.issues += 1
-            }
-        })
-    })
-
-    return {
-        totalCommits: totalCommitCount,
-        totalIssues: totalIssueCount,
-        totalTests: totalTestCount,
-        teamMembers: teamInfo,
-    }
-}
-
+// Adapted from Electrends https://gitlab.com/dandom25/electrends/
 const About = () => {
-    const [teamList] = useState([])
-    const [totalCommits, setTotalCommits] = useState(0)
-    const [totalIssues, setTotalIssues] = useState(0)
-    const [totalTests, setTotalTests] = useState(0)
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [numCommits, setNumCommits] = useState(0);
+  const [numIssues, setNumIssues] = useState(0);
+  const [numTests, setNumTests] = useState(0);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            if (teamList === undefined || teamList.length === 0) {
-                const gitlabInfo = await getGitLabInfo()
-                setTotalCommits(gitlabInfo.totalCommits)
-                setTotalIssues(gitlabInfo.totalIssues)
-                setTotalTests(gitlabInfo.totalTests)
-            }
-        }
-        fetchData()
-    }, [teamList])
+  useEffect(() => {
+    const getData = async () => {
+      if (teamMembers === undefined || teamMembers.length === 0) {
+        const stats = await getGitLabStats();
+        setTeamMembers(stats.teamMembers);
+        setNumCommits(stats.numCommits);
+        setNumIssues(stats.numIssues);
+        setNumTests(stats.numTests);
+      }
+    };
+    getData();
+  }, [teamMembers]);
 
+  return (
+    <Container>
+      <h1>About Us</h1>
+      <Container>
+        <p>
+          Description
+        </p>
+      </Container>
+      <h1>Team Members</h1>
+      <Row className="g-3 m-0 justify-content-center" xs="auto" md={5}>
+        {teamMembers.map((member) => {
+          return (
+            <Col key={member.name} as="div">
+              <Card className="bioCard" style={{ width: "16rem" }}>
+                <Card.Img className="bioPic" variant="top" src={member.picture} />
+                <Card.Body>
+                  <Card.Title>{member.name}</Card.Title>
+                  <Card.Subtitle>{member.role}</Card.Subtitle>
+                  <Card.Text>{member.bio}</Card.Text>
+                  <ListGroup variant="flush">
+                    <ListGroupItem>Commits: {member.commits}</ListGroupItem>
+                    <ListGroupItem>Issues: {member.issues}</ListGroupItem>
+                    <ListGroupItem>Tests: {member.tests}</ListGroupItem>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
 
-    return (
-        <div className="About">
-            <header className="About-header">
-                <h1> About </h1>
-            </header>
-            <header className="About-description">
-                <h3> Description </h3>
-                <p>HomePlanet is a website that provides astronomical data in an easily-accessible format. </p>
-                <p>Specifically, we focus on the habitability of Planets/Exoplanets and provide information on their stars and moons.</p>
-                
-            </header>
-            <h4><br></br></h4>
-            <header className="Members">
-                <h3> Members </h3>
-            </header>
-            <> <Row className="g-3 m-0 justify-content-center" xs="auto" md={5}>
-                    {teamInfo.map((member) => {
-                        return (
-                            <Col key={member.name} as="div" xs="auto" mx-auto>
-                                <Card style={{ width: '18rem' }}>
-                                    <Card.Body>
-                                        <Card.Img variant="top" src={member.picture}></Card.Img>
-                                        <Card.Title>{member.name}</Card.Title>
-                                        <Card.Subtitle>{member.role}</Card.Subtitle>
-                                        <Card.Text>{member.bio}</Card.Text>
-                                    </Card.Body>
-                                    <ListGroup variant="flush">
-                                        <ListGroupItem>Commits: {member.commits}</ListGroupItem>
-                                        <ListGroupItem>Issues: {member.issues}</ListGroupItem>
-                                        <ListGroupItem>Tests: {member.tests}</ListGroupItem>
-                                    </ListGroup>
-                                </Card>
-                            </Col>
-                        )})}
-                </Row> </>
-            <h1><br></br></h1>
-            <header className="Gitlab Stats">
-                <h3> Repository Statistics </h3>
-            </header>
-            <> <Row className="g-3 m-0 justify-content-center" xs="auto" >
-                    <Col key="totalCommits" as="div" xs="auto" mx-auto>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Body>
-                                <Card.Title>Total commits:</Card.Title>
-                                <Card.Text>{totalCommits}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col key="totalIssues" as="div" xs="auto" mx-auto>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Body>
-                                <Card.Title>Total issues:</Card.Title>
-                                <Card.Text>{totalIssues}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col key="totalTests" as="div" xs="auto" mx-auto>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Body>
-                                <Card.Title>Total tests:</Card.Title>
-                                <Card.Text>{totalTests}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </>
-            <h1><br></br></h1>
+      <h1>Repository Statistics</h1>
+      <Container>
+        <div className="repoStats">
+          <Table>
+            <thead>
+              <tr>
+                <th>Total Commits</th>
+                <th>Total Issues</th>
+                <th>Total Tests</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{numCommits}</td>
+                <td>{numIssues}</td>
+                <td>{numTests}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+        <h1><br></br></h1>
             <header className="Tools">
                 <h3> Tools Utilized </h3>
             </header>
@@ -202,8 +214,9 @@ const About = () => {
                     </Col>
                 </Row>
             </>
-        </div>
-    )
-}
-
-export default About
+        
+      </Container>
+    </Container>
+  );
+};
+export default About;
