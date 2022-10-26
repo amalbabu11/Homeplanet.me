91d38f2994f33cfe19b27aaa8662c48a44b432ce
Summer Ely <summerely@Summers-MacBook-Pro-2.local>
2022-10-26 00:17:19 -0500

    Added missing import

--- frontend/src/__tests__/JestTests.test.js
+++ frontend/src/__tests__/JestTests.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BrowserRouter from "react-router-dom";
 // import OurNavbar from "../components/OurNavbar";
+import App from "../App";
 
 // Credit: https://gitlab.com/JohnPowow/animalwatch/-/blob/main/frontend/src/__tests__/JestTests.test.js
 
